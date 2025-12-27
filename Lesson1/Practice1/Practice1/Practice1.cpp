36de098b5497ff1b939819a740553d4625379348
whyk0426 <kkyyss426@gmail.com>
2025-12-27 18:08:48 +0900

    중꺽마

---
+++ Lesson1/Practice1/Practice1/Practice1.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+struct Music
+{
+	std::string name;
+	Music* prev;
+	Music* next;
+};
+
+class Playlist 
+{
+ private:
+	Music* head;
+	Music* current;
+	Music* tail;
+
+public:
+	void add(std::string name) {
+		if (head == NULL) {
+			Music* new_music = new Music{ name, nullptr, nullptr };
+			new_music->prev = new_music;
+			new_music->next = new_music;
+			head = new_music;
+			current = new_music;
+			tail = new_music;
+		}
+		else {
+			Music* new_music = new Music{ name, nullptr, nullptr };
+		}
+
+	}
+	void remove() {
+
+	}
+		
+};
