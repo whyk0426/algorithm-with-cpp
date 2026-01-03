95c16bfaa5942cc26cd7d1c949ff7905615afa75
whyk0426 <kkyyss426@gmail.com>
2026-01-03 18:14:46 +0900

    practice4는 진짜 모르겠다

--- Lesson2/Practice4/Practice4/Practice4.cpp
+++ Lesson2/Practice4/Practice4/Practice4.cpp
@@ -1,20 +1,45 @@
-﻿// Practice4.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
+﻿#include <iostream>
+#include <vector>
 
-#include <iostream>
+struct File
+{
+    std::string name;
+    bool is_dir;
+    std::vector<File*> children;
+};
+
+struct File_system 
+{
+private:
+    File* root;
+    File* cwd;
+public:
+    File_system() {
+        root = new File{ "/", true, {} };
+        cwd = root;
+    }
+    File* find(const std::string& path) {
+        if (path[0] == '/') {
+            return find_impl(root, path.substr(1));
+        }
+        else {
+            return find_impl(cwd, path);
+        }
+    }
+
+private:
+    File* find_impl(File* directory, const std::string& path) {
+        if (path.empty())
+            return directory;
+        auto sep = path.find('/');
+        std::string current_path = sep == std::string::npos ? path : path.substr(0, sep);
+        std::string rest_path = sep == std::string::npos ? "" : path.substr(sep + 1);
+
+    }
+};
+
 
 int main()
 {
     std::cout << "Hello World!\n";
 }
-
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
-
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
