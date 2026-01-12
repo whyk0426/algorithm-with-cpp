d7c9421243a5c7b98b85e5f0141a118f17d588ca
whyk0426 <kkyyss426@gmail.com>
2026-01-12 11:31:01 +0900

    다운 받아야해서 건너뜀!

---
+++ Lesson3/Practice7/Practice7/Practice7.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <vector>
+
+class BloomFilter {
+private:
+    std::vector<bool> data;
+    int N;
+
+public:
+    BloomFilter(int n) : N(n) {
+        data = std::vector<bool>(n, false);
+    }
+
+
+};
+
+int main()
+{
+    std::cout << "Need to download for conclude!!!\n";
+}
